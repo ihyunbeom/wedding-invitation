2e20d4f99f4ab537154660b6ae5287e11a538291
LeeHyunBeom <ihyunbeom@gmail.com>
2025-09-18 20:52:01 +0900

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -12,7 +12,7 @@ export { dayjs }
 export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
+export const LOCATION = "라마다 서울 신도림 호텔 하늘정원홀"
 export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
 
 export const SHARE_ADDRESS = LOCATION
@@ -23,11 +23,11 @@ export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 export const NMAP_PLACE_ID = 13321741
 export const KMAP_PLACE_ID = 8634826
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
+export const BRIDE_FULLNAME = "이아람"
+export const BRIDE_FIRSTNAME = "아람"
 export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FATHER = "ㅇㅇㅇ"
+export const BRIDE_MOTHER = "ㅇㅇㅇ"
 export const BRIDE_INFO = [
   {
     relation: "신부",
@@ -49,11 +49,11 @@ export const BRIDE_INFO = [
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
+export const GROOM_FULLNAME = "이현범"
+export const GROOM_FIRSTNAME = "현범"
 export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FATHER = "이계현"
+export const GROOM_MOTHER = "이현숙"
 export const GROOM_INFO = [
   {
     relation: "신랑",
