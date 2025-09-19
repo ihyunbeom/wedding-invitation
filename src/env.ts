f885b8b892b84a8ea0fbb34ab0519c62a93b8a97
HyunBeom Lee <ihyunbeom@gmail.com>
2025-09-19 12:18:36 +0900

    ci: naver client add

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-export const NAVER_MAP_CLIENT_ID = "xtb0n4i1l9"
+export const NAVER_MAP_CLIENT_ID = import.meta.env.VITE_NAVER_MAP_CLIENT_ID
 export const KAKAO_SDK_JS_KEY = import.meta.env.VITE_KAKAO_SDK_JS_KEY
-export const SERVER_URL = "https://github.com/ihyunbeom/wedding-invitation-server"
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 export const STATIC_ONLY = import.meta.env.VITE_STATIC_ONLY === "true"
