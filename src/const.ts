ac655d7550f0c97cde3df0632c5dc1186d88b201
HyunBeom Lee <ihyunbeom@gmail.com>
2025-09-19 12:24:35 +0900

    ci: naver client add 2

--- src/const.ts
+++ src/const.ts
@@ -18,9 +18,9 @@ export const LOCATION_ADDRESS = "서울시 구로구 경인로 624"
 export const SHARE_ADDRESS = LOCATION
 export const SHARE_ADDRESS_TITLE = LOCATION
 
-export const WEDDING_HALL_POSITION = [126.885328, 37.5063294]
+export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 
-export const NMAP_PLACE_ID = 13321741
+export const NMAP_PLACE_ID = 986184099
 export const KMAP_PLACE_ID = 8634826
 
 export const BRIDE_FULLNAME = "이아람"
